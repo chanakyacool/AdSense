724f416992e219263eb92f738ac1f7714cf83db6
chanakyacool <chanakyadevraj@yahoo.com>
2014-08-23 13:23:15 +0530

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdSense::Application.config.secret_token = 'ee42c23601b2d22cb3a17c940d7265dafb69580311e3213786b27d7f63ef70a31ed32fd25aa271a0c80336fdfb23ee007ad3f9f97c012b00c61bb61011dff677'
