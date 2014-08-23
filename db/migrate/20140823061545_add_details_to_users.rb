e67cfec383ec22913c0cf73d42072ca5a7f699d3
chanakyacool <chanakyadevraj@yahoo.com>
2014-08-23 13:59:19 +0530

    latest

--- db/migrate/20140823061545_add_details_to_users.rb
+++ db/migrate/20140823061545_add_details_to_users.rb
@@ -2,5 +2,11 @@ class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :dob, :date
     add_column :users, :username, :string
+    add_column :users, :gender, :string
+    add_column :users, :economy, :string
+	add_column :users, :designation, :string
+    add_column :users, :country, :string
+    add_column :users, :state, :string
+    add_column :users, :city, :string
   end
 end
